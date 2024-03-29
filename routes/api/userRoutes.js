ce925805d38e95eae6ac66ad7bb37278bbeddc85
Tyler Clark <tylerc111@gmail.com>
2024-03-29 16:59:05 -0400

    added express router functionality to router/index.js and routes to userRoutes.js in the api folder

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// GET all users
+router.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// GET a single user by its _id and populated thought and friend data
+router.get('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).populate('thoughts').populate('friends');
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// POST a new user 
+router.post('/api/users', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// PUT to update a user by their _id
+router.put('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// DELETE to remove a user by their _id
+router.delete('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        // Bonus: Remove user's associated thoughts
+        await Thought.deleteMany({ _id: { $in: user.thoughts } });
+        res.json({ message: "User deleted" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
